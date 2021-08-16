78769c6d81f328e82c8bce79487cf811c924b85a
Admin <Admin@WIN-1NFJ5HHJQS8>
2021-08-16 09:15:43 +0430

    Add project files.

---
+++ RegisterPostalAddresses/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegisterPostalAddresses.Models
+{
+    public class Product
+    {
+        public Guid id { get; set; }
+
+        public string ProductName { get; set; }
+
+
+        public string ProductImage { get; set; }
+
+        public int Price { get; set; }
+
+    }
+}
---
+++ RegisterPostalAddresses/Services/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RegisterPostalAddresses.Models;
+
+namespace RegisterPostalAddresses.Services
+{
+    public class ProductService : IProductService
+    {
+         async Task<List<Product>> IProductService.GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
